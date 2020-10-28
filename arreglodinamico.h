b606fc52eeeee4723d39d88256d6e60548741f5c
Diana Castillo <diana.castillo7676@alumnos.udg.mx>
2020-10-27 19:09:21 -0600

    Reimplementacion de clase ArregloDinamico usando templates

--- arreglodinamico.h
+++ arreglodinamico.h
@@ -5,10 +5,11 @@
 
 using namespace std;
 
+template <class T>
 class ArregloDinamico
 {
     private:
-        string *arreglo;
+        T *arreglo;
         size_t tam;
         size_t cont;
         const static size_t MAX = 5;
@@ -17,12 +18,117 @@ class ArregloDinamico
     public:
         ArregloDinamico();
         ~ArregloDinamico();
-        void insertar_final(const string &s);
-        void insertar_inicio(const string &s);
+        void insertar_final(const T&);
+        void insertar_inicio(const T&);
+        void insertar(const T&, size_t);
+
+        void eliminar_final();
+        void eliminar_inicio();
+        void eliminar(size_t);
+
         size_t size();
-        string operator[](size_t p) {
+        T operator[](size_t p) {
             return arreglo[p];
         }
 };
+
+template <class T>
+ArregloDinamico<T>::ArregloDinamico() {
+    arreglo = new T[MAX];
+    cont = 0;
+    tam = MAX;
+}
+
+template <class T>
+ArregloDinamico<T>::~ArregloDinamico() {
+    delete[] arreglo;
+}
+
+template <class T>
+void ArregloDinamico<T>::insertar_final(const T& s) {
+    if(cont == tam)
+        expandir();
+    
+    arreglo[cont] = s;
+    cont++;
+}
+
+template <class T>
+void ArregloDinamico<T>::insertar_inicio(const T& s) {
+    if(cont == tam)
+        expandir();
+
+    for(size_t i=cont; i>0; i--)
+        arreglo[i] = arreglo[i-1];
+
+    arreglo[0] = s;
+    cont++;
+}
+
+template <class T>
+void ArregloDinamico<T>::insertar(const T& s, size_t p) {
+    if (p >= cont) {
+        cout<<"Posicion invalida"<<endl;
+        return;
+    }
+    if(cont == tam)
+        expandir();
+
+    for (size_t i = cont; i > p; i--)
+        arreglo[i] = arreglo[i-1];
+    
+    arreglo[p] = s;
+    cont++;
+}
+
+template <class T>
+void ArregloDinamico<T>::eliminar_final() {
+    if(cont == 0) {
+        cout<<"Arreglo vacio"<<endl;
+        return;
+    }
+    cont--;
+}
+
+template <class T>
+void ArregloDinamico<T>::eliminar_inicio() {
+    if(cont == 0) {
+        cout<<"Arreglo vacio"<<endl;
+        return;
+    }
+    for(size_t i=0; i<cont-1; i++)
+        arreglo[i] = arreglo[i+1];
+    
+    cont--;
+}
+
+template <class T>
+void ArregloDinamico<T>::eliminar(size_t p) {
+    if(cont == 0) {
+        cout<<"Arreglo vacio"<<endl;
+        return;
+    }
+    for(size_t i=p; i<cont-1; i++)
+        arreglo[i] = arreglo[i+1];
+
+    cont--;
+}
+
+template <class T>
+void ArregloDinamico<T>::expandir() {
+    T *nuevo = new T[tam+MAX];
+
+    for(size_t i=0; i<cont; i++)
+        nuevo[i] = arreglo[i];
+
+    delete[] arreglo;
+    arreglo = nuevo;
+    tam += MAX;
+}
+
+template <class T>
+size_t ArregloDinamico<T>::size() {
+    return cont;
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -4,22 +4,26 @@
 using namespace std;
 
 int main() {
-    ArregloDinamico arreglo;
+    ArregloDinamico<string> arreglo;
     arreglo.insertar_final("Diana");
-    arreglo.insertar_final("Diana");
-    arreglo.insertar_final("Diana");
-    arreglo.insertar_final("Diana");
-    arreglo.insertar_final("Diana");
-    arreglo.insertar_final("Diana");
-    arreglo.insertar_final("Diana");
-    arreglo.insertar_final("Diana");
-    
-    for(size_t i=0; i<arreglo.size(); i++)
-        cout<<arreglo[i]<<" ";
-
-    cout<<endl;
+    arreglo.insertar_final("Fernanda");
     arreglo.insertar_inicio("Rebolledo");
     arreglo.insertar_inicio("Castillo");
+    
+    cout<<endl;
+    for(size_t i=0; i<arreglo.size(); i++)
+        cout<<arreglo[i]<<" ";
+    cout<<endl<<endl;
+
+    arreglo.insertar("Holi", 2);
+
+     for(size_t i=0; i<arreglo.size(); i++)
+        cout<<arreglo[i]<<" ";
+    cout<<endl;
+
+    arreglo.eliminar_inicio();
+    arreglo.eliminar_final();
+    arreglo.eliminar(1);
 
     for(size_t i=0; i<arreglo.size(); i++)
         cout<<arreglo[i]<<" ";
